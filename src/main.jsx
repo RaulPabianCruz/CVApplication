bb6be8891ab3a750f39bdbee3de378c2fbfe9bd7
RaulPabianCruz <ito.pabian@gmail.com>
2024-05-08 16:06:52 -0700

    Update main.jsx component imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import ContactInfo from './ContactInfo';
-import Summary from './Summary';
-import Education from './Education';
-import Practical from './Practical';
+import ContactInfo from './components/ContactInfo';
+import Summary from './components/Summary';
+import Education from './components/Education';
+import Practical from './components/Practical';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
